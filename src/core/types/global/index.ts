72c1fb4cc889b3068b51bcc6f842c3d03b69e493
Emmanuel Olubiyi <42422184+coded1guy@users.noreply.github.com>
2024-10-10 12:46:53 +0100

    fix: type error in src/core/types/global

--- src/core/types/global/index.ts
+++ src/core/types/global/index.ts
@@ -2,7 +2,7 @@ import { IJwtData } from "../../../auth/types";
 declare global {
   namespace Express {
     export interface Request {
-      user: IJwtData | null | undefined;
+      user?: IJwtData | null | undefined;
     }
   }
 }
